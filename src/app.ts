7e8ee383f28ef8cacf546961940c64a10ebd5f92
Alex Santos de Souza <alexssmusica@gmail.com>
2021-03-02 16:24:51 -0300

    fix: ajustes na pagina inicial

--- src/app.ts
+++ src/app.ts
@@ -27,9 +27,38 @@ app.use(
         });
     }
 );
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-  })
+
+app.get('/', function(req: Request, res: Response) {
+    
+    res.send(`
+    <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>NPS - API</title>
+  </head>
+  <body>
+    
+    <div class="container-fluid bg-primary p-3 mb-2 text-white text-center"">
+    <h1>NPS API</h1>
+    <h5>Uma Api para pesquisa de satisfação sobre Produtos e Serviços.</h>
+    </div>
+ 
+
+   
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+  `);
+});
 
 export { app };
 
